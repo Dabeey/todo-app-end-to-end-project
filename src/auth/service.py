8bef179ac49d34baa27886b1844a50827cc5cb6d
Dabeey <abbasdaughter18@gmail.com>
2025-10-01 21:52:53 +0100

    service.py: hash password, verify hashed password, authenticate user, create and verify access token

--- src/auth/service.py
+++ src/auth/service.py
@@ -15,4 +15,42 @@ import os
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 ALGORITHM = os.getenv('ALGORITHM')
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
+
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl='auth/token')
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return bcrypt_context.verify(plain_password, hashed_password)
+
+def get_password_hash(password: str) -> str:
+    return bcrypt_context.hash(password)
+
+
+def authenticate_user(email:str, password:str, db:Session) -> User | bool:
+    user = db.query(User).filter(User.email == email).first()
+    if not user or not verify_password(password, user.password_hash):
+        logging.warning(f'Failed to authenticate email for {email}')
+        return False
+    return user
+
+
+def create_access_token(email:str, user_id: UUID, expires_delta: timedelta) -> str:
+    encode = {
+        'sub': email,
+        'id': str(user_id),
+        'exp': datetime.now(timezone.utc) + expires_delta
+    }
+    return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+def verify_token(token: str) -> schemas.TokenData:
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
+        user_id: str = payload.get('id')
+        return schemas.TokenData(user_id=user_id)
+    except PyJWTError as e:
+        logging.warning(f'Token verification failed: {str(e)}')
+        raise AuthenticationError()
