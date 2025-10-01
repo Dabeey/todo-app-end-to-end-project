f1de0d701309deeadcdeafc39a5817d2aa774c40
Dabeey <abbasdaughter18@gmail.com>
2025-10-01 20:20:46 +0100

    core.py: Add session

--- src/database/core.py
+++ src/database/core.py
@@ -8,3 +8,18 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL = os.getenv('DATABASE_URL')
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+DbSession = Annotated(Session, Depends(get_db))
