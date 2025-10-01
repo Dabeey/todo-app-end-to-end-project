1996a73f0971d7f958d736eef6d5f186c6119171
Dabeey <abbasdaughter18@gmail.com>
2025-10-01 22:27:06 +0100

    update controller.py with db session

--- src/auth/controller.py
+++ src/auth/controller.py
@@ -1,6 +1,7 @@
 from typing import Annotated
 from fastapi import APIRouter, status, Request, Depends
 from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.orm import Session
 from ..database.core import DbSession
 from ..rate_limiter import limiter
 from . import schemas
@@ -13,10 +14,10 @@ router = APIRouter(
 
 @router.post('/', status_code=status.HTTP_201_CREATED)
 @limiter.limit('5/hour')
-async def register_user(request: Request, db: DbSession, register_user_request: schemas.RegisterUserRequest):
+async def register_user(request: Request, register_user_request: schemas.RegisterUserRequest, db: Session = DbSession):
     service.register_user(db, register_user_request)
 
 
 @router.post('/token', response_model=schemas.Token)
-async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends() ], db: DbSession):
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends() ], db =  DbSession):
     return service.login_for_access_token(form_data, db)
