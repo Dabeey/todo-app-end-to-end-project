e16b0741d2e3241fb78ad6718177decd3175a657
Dabeey <abbasdaughter18@gmail.com>
2025-10-05 15:33:44 +0100

    Add user error exceptions

--- src/exceptions.py
+++ src/exceptions.py
@@ -14,3 +14,26 @@ class TodoCreationError(TodoError):
         super().__init__(status_code=500, detail=f'Failed to create todo: {error}')
 
 
+""" User Error"""
+class UserError(HTTPException):
+    pass
+
+
+class UserNotFoundError(UserError):
+    def __init__(self, user_id=None):
+        message = 'User not found' if user_id is None else f'User with id {user_id} not found'
+        super().__init__(status_code=404, detail=message)
+
+
+class PasswordMismatchError(UserError):
+    def __init__(self):
+        super().__init__(status_code=400, detail='New Passwords  do not match')
+
+
+class InvalidPasswordError(UserError):
+    def __init__(self):
+        super.__init__(status_code=401, detail='Current pasword is incorrect')
+
+class AuthenticationError(HTTPException):
+    def __init__(self, status_code=401, detail = 'Could not validate user'):
+        super().__init__(status_code, detail)
